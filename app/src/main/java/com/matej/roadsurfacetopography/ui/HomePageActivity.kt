5e0aa4c550193c4801da0575317881ffc13af71a
Matej Milić <mmilic@etfos.hr>
2020-04-14 18:50:13 +0200

    added base activity

---
+++ app/src/main/java/com/matej/roadsurfacetopography/ui/HomePageActivity.kt
@@ -0,0 +1,14 @@
+package com.matej.roadsurfacetopography.ui
+
+import com.matej.roadsurfacetopography.R
+import com.matej.roadsurfacetopography.ui.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_homepage.*
+
+class HomePageActivity : BaseActivity() {
+    override fun getLayoutResourceId(): Int = R.layout.activity_homepage
+
+    override fun setupUi() {
+    }
+
+
+}
---
+++ app/src/main/java/com/matej/roadsurfacetopography/ui/base/BaseActivity.kt
@@ -0,0 +1,16 @@
+package com.matej.roadsurfacetopography.ui.base
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutResourceId())
+        setupUi()
+    }
+
+    abstract fun getLayoutResourceId(): Int
+    abstract fun setupUi()
+}
